dfe73ce4c5dfc9e98fa1c45ee10189a9c017cd09
bushido117 <vadim.saiko53370@mail.ru>
2022-08-24 12:55:22 +0300

    Initial Commit

---
+++ Delegate_HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegate_HW
+//
+//  Created by Вадим Сайко on 24.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
